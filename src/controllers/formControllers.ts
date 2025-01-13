fed2a24c13905dccc86b8d351b1548c99fac8655
Vinay chaudhary <vinay.heyworld@gmail.com>
2025-01-13 22:51:42 +0530

    feat: add getFormById controller

--- src/controllers/formControllers.ts
+++ src/controllers/formControllers.ts
@@ -1,3 +1,7 @@
+import { Context } from "hono";
+import { responseHandler } from "../utils/response";
+import { createClient } from "../db/database";
+
 export const createForm = async () => {
 
 };
@@ -6,8 +10,58 @@ export const getFormWithPage = async () => {
 
 };
 
-export const getFormById = async () => {
+export const getFormById = async (c: Context) => {
+        try {
+            
+            const formId = c.req.param('formId');
+            if(!formId){
+                return c.json(responseHandler('error', 'formId is missing'), 401); 
+            }
 
+            const { DATABASE_URL } = c.env;
+            if (!DATABASE_URL) {
+              return c.json( responseHandler("error", "Server configuration error"), 500 );
+            };
+    
+            const db = createClient(DATABASE_URL); 
+
+            const form = await db.form.findFirst({
+                where: {
+                    id : formId,
+                },
+                select: {
+                    id: true, 
+                    title: true, 
+                    description: true, 
+                    status: true,
+                    pages: {
+                        select: {
+                            id: true, 
+                            page: true,
+                            content: true,
+                        },orderBy: {
+                            page: 'asc'
+                        }
+                    }
+                }
+            }); 
+
+            if(!form){
+                return c.json(responseHandler('error', 'Form not found'), 404); 
+            }
+            
+            if(!form.status){
+                return c.json(responseHandler('error', 'Form is not published'), 403); 
+            }
+
+            return c.json(responseHandler('success', 'Form fetched successfully', form), 200); 
+
+        } catch (error) {
+            return c.json(responseHandler('error', 'Failed to fetch form', {
+                error : error instanceof Error ? error.message : 'Internal server error'
+            }), 
+        500); 
+        }
 };
 
 export const updateFormPage = async () => {
