24bf31ef0efee2786ad1e81a3a5c005628e7ab36
Vinay chaudhary <vinay.heyworld@gmail.com>
2025-01-13 23:29:27 +0530

    feat: add createForm controller

--- src/controllers/formControllers.ts
+++ src/controllers/formControllers.ts
@@ -1,9 +1,59 @@
 import { Context } from "hono";
 import { responseHandler } from "../utils/response";
 import { createClient } from "../db/database";
+import { createFormSchema } from "../utils/zodSchemas";
 
-export const createForm = async () => {
+export const createForm = async (c: Context) => {
+    try {
+        const body = await c.req.json(); 
+        const user = c.get('user');
 
+        const formData = createFormSchema.safeParse(body);
+        if (!formData.success) {
+            return c.json(responseHandler("error", "Invalid input"), 400);
+        };
+
+        const { title, description } = formData.data; 
+
+        const { DATABASE_URL } = c.env;
+        if (!DATABASE_URL) {
+          return c.json( responseHandler("error", "Server configuration error"), 500 );
+        };
+
+        const db = createClient(DATABASE_URL); 
+
+        const [form, page] = await db.$transaction(async(t) => {
+            const form = await t.form.create({
+                data: {
+                    title,
+                    description,
+                    userId: user.id
+                }
+            })
+
+            const page = await t.formPage.create({
+                data: {
+                    page: 1,
+                    content: null,
+                    formId: form.id
+                }
+            })
+
+            return [form, page]; 
+        })
+
+        if(!form){
+            return c.json(responseHandler('error', 'Failed to create form'), 500); 
+        }
+
+        return c.json(responseHandler('success', 'Form created successsfully', form), 200); 
+        
+    } catch (error) {
+        return c.json(responseHandler('error', 'Failed to create form', {
+            error : error instanceof Error ? error.message : 'Internal server error'
+        }), 
+    500); 
+    }
 };
 
 export const getFormWithPage = async () => {
@@ -12,7 +62,7 @@ export const getFormWithPage = async () => {
 
 export const getFormById = async (c: Context) => {
         try {
-            
+
             const formId = c.req.param('formId');
             if(!formId){
                 return c.json(responseHandler('error', 'formId is missing'), 401); 
