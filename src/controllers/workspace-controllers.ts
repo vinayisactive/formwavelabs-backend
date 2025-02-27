6f72561fae26a9c84f7a93c705a561c5efe0d433
Vinay chaudhary <vinay.heyworld@gmail.com>
2025-02-27 23:45:48 +0530

    feat: add invite-member (with role) controller/handler with validations and important checks (time limit)

--- src/controllers/workspace-controllers.ts
+++ src/controllers/workspace-controllers.ts
@@ -2,6 +2,7 @@ import { Context } from "hono";
 import { withGlobalErrorHandler } from "../utils/global-error-handler";
 import { handleResponse } from "../utils/response-handler";
 import { getDatabase } from "../db/database";
+import { v4 as uuidv4 } from "uuid";
 
 export const createWorkspace = withGlobalErrorHandler(async (c: Context) => {
   const user = c.get("user");
@@ -321,6 +322,148 @@ export const updateWorkspace = withGlobalErrorHandler(async (c: Context) => {
       "success",
       "Application: Workspace name updated.",
       workspace
+    ),
+    200
+  );
+});
+
+export const inviteMember = withGlobalErrorHandler(async (c: Context) => {
+  const user = c.get("user");
+  const workspaceId = c.req.param("workspaceId");
+  const { email, role } = await c.req.json();
+
+  if (!workspaceId) {
+    return c.json(
+      handleResponse("error", "Missing param: workspace ID required."),
+      400
+    );
+  }
+
+  if(!email ||  !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
+    return c.json(
+      handleResponse("error", "Invalid input: Email is invalid or missing."),
+      400
+    );
+  }
+
+  if (!role) {
+    return c.json(
+      handleResponse("error", "Invalid input: Role is missing."),
+      400
+    );
+  }
+
+  const { DATABASE_URL } = c.env;
+  if (!DATABASE_URL) {
+    return c.json(
+      handleResponse(
+        "error",
+        "Server misconfiguration: Missing database URL or RESEND key."
+      ),
+      500
+    );
+  }
+
+  const db = getDatabase(DATABASE_URL);
+
+  const member = await db.workspaceMember.findFirst({
+    where: {
+      workspaceId,
+      userId: user.id,
+    },
+  });
+
+  if (!member) {
+    return c.json(
+      handleResponse(
+        "error",
+        "Unauthorized: You are not a member of this workspace."
+      ),
+      403
+    );
+  }
+
+  if (!["OWNER", "ADMIN"].includes(member.role)) {
+    return c.json(
+      handleResponse(
+        "error",
+        "Forbidden: Insufficient permissions to invite members."
+      ),
+      403
+    );
+  }
+
+  const existingUser = await db.user.findFirst({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+      email: true,
+    },
+  });
+
+
+  if (!existingUser) {
+    return c.json(
+      handleResponse("error", "Application: Cannot invite, user does not exist."),
+      400
+    );
+  }
+
+  const existingValidInvitation  = await db.invitation.findFirst({
+    where: {
+      email,
+      workspaceId,
+      expiresAt: {gt: new Date()}
+    },
+    select: {
+      id: true,
+      expiresAt: true
+    },
+  });
+
+  if (existingValidInvitation) {
+    return c.json(
+      handleResponse(
+        "error",
+        "Appliation: Active invitation already exists for this user."
+      ),
+      409
+    );
+  } 
+
+  const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+
+  const invitation = await db.invitation.create({
+    data: {
+      email,
+      userId: existingUser.id,
+      role,
+      token : uuidv4(),
+      expiresAt,
+      workspaceId,
+    },
+    select: {
+      id: true,
+      email: true,
+      workspaceId: true,
+      status: true,
+    }
+  });
+
+  if (!invitation) {
+    return c.json(
+      handleResponse("error", "Server: Failed to create invitation."),
+      500
+    );
+  }
+
+  return c.json(
+    handleResponse(
+      "success",
+      "Application: Invitation sent successfully.",
+      invitation
     ),
     200
   );
